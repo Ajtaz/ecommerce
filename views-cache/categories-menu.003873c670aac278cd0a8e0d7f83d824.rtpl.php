4884bbff616212fa54d632432644156f9186cd74
Aurelio Azevedo <aureliojazevedo@gmail.com>
2019-12-12 11:16:07 -0300

    24-108. Site Categorias

---
+++ views-cache/categories-menu.003873c670aac278cd0a8e0d7f83d824.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Brinquedos</a></li><li><a href="/categories/8">Computadores</a></li><li><a href="/categories/6">Eletrônicos</a></li>
