8730f99231b2b630567f203ab7db07ac65fe37c1
Aurelio Azevedo <aureliojazevedo@gmail.com>
2019-12-26 19:03:44 -0300

    24-113: Site - Detalhes do Produto

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -105,7 +105,7 @@ class Product extends Model {
 	}
 
 
-public function setPhoto($file)
+	public function setPhoto($file)
 	{
 			$extension = explode('.', $file['name']);  // cria um array 
 			$extension = end($extension);
@@ -136,6 +136,26 @@ public function setPhoto($file)
 			imagedestroy($image);
 			
 			$this->checkPhoto();
+	}
+
+
+	public function getFromURL($desurl)
+	{
+		$sql = new Sql();
+		$rows = $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1", [
+			':desurl'=>$desurl
+		]);
+		$this->setData($rows[0]);
+	}
+
+	public function getCategories()
+	{
+		$sql = new Sql();
+		return $sql->select("
+			SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct = :idproduct
+		", [
+			':idproduct'=>$this->getidproduct()
+		]);
 	}
 
 }
